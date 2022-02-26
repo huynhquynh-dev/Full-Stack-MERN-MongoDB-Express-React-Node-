4c7f83c706c26bd5a7aed4258c710a4f1265f2b1
huynhquynh-dev <huynhquynh-dev>
2022-02-26 21:33:42 +0700

    -Add auth login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -47,4 +47,46 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Simple validation
+    if (!username || !password)
+        return res
+            .status(400)
+            .json({ success: false, message: 'Missing username and/or password' });
+
+    try {
+        // Check for existing user
+        const user = await User.findOne({ username });
+        if (!user)
+            return res
+                .status(400)
+                .json({ success: false, message: 'Incorrect username or password' });
+
+        // Username found
+        const passwordValid = await argon2.verify(user.password, password);
+        if (!passwordValid)
+            return res
+                .status(400)
+                .json({ success: false, message: 'Incorrect username or password' });
+
+        // All good
+        // Return token
+        const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET);
+
+        res.json({
+            success: true,
+            message: 'User logged in successfully',
+            accessToken,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
 module.exports = router;
