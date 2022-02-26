1afb774050300f9ce4f3f68bf7e9badcbc4535e6
huynhquynh-dev <huynhquynh-dev>
2022-02-26 16:39:07 +0700

    -Add connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://lionhuynh:quynh.huynh@cluster0.sxcoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        );
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 
