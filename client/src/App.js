092c33226d6bceb47139a15125fc0d28bae29fd2
huynhquynh-dev <huynhquynh-dev>
2022-02-27 10:46:44 +0700

    -Add router auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./views/Auth";
+import Landing from "./components/layout/Landing";
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Auth authRoute="login" />} />
+                <Route
+                    path="/register"
+                    element={<Auth authRoute="register" />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
